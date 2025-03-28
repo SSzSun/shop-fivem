db318748d522505bcaeabd9fc4181f9b86d6cb50
SSzSun <jaruphat.sun@gmail.com>
2025-03-29 04:55:29 +0700

    refactor: update ESLint configuration and improve code formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import React from 'react';
+
 export default function Home() {
-  return (
-    <>
-      <h1 className="text-xs font-bold underline">Hello world!</h1>
-    </>
-  );
+	return (
+		<div>
+			<h1 className="text-xs font-bold underline">
+        			Hello world!
+			</h1>
+		</div>
+	);
 }
